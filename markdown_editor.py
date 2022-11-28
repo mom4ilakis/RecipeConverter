a97f90cdd6dc5046bf04c8bf5fca4c2638e05be0
Momchil Marinov <mom4ilakis@gmail.com>
2022-11-29 00:55:14 +0200

    Add support for template

--- markdown_editor.py
+++ markdown_editor.py
@@ -16,37 +16,75 @@ def count_files(path: Path, filename: str):
 
 
 class MarkdownEditor:
-    def __init__(self, title):
+    def __init__(self, title, path_to_template, use_template=True):
         self.markdown = ""
         self.title = title
+        self.use_template = use_template
+
+        with open(path_to_template) as template_file:
+            self.template = template_file.read()
 
     def add_title(self, title: str):
-        self.markdown += f'# {title.capitalize()}\n'
+        if self.use_template:
+            self.template = self.template.replace('<!--title-->', title)
+        else:
+            self.markdown += f'# {title.capitalize()}\n'
 
     def add_tags(self, tags):
-        self.markdown += f'''---\n\ttags: [{tags}]\n---\n'''
+        if self.use_template:
+            self.template = self.template.replace('<!--tags_list-->', tags)
+        else:
+            self.markdown += f'''---\n\ttags: [{tags}]\n---\n'''
 
     def add_ingredients(self, ingredients: dict):
-        self.markdown += '## Ingredients\n'
+        text = ''
+
         for component, ingr_list in ingredients:
-            self.markdown += f'#### {component}'
+            text += f'#### {component}'
 
             for amount, measurement, ingredient in ingr_list:
-                self.markdown += f'* {amount}{measurement} {ingredient}\n'
+                text += f'* {amount}{measurement} {ingredient}\n'
 
-        self.markdown += '---\n'
+        if self.use_template:
+            self.template = self.template.replace('<!--ingredients_list-->', text)
+        else:
+            self.markdown += '## Ingredients\n'
+            self.markdown += text
+            self.insert_line()
 
     def add_instructions(self, instructions: dict):
-        self.markdown += instructions
-        self.insert_line()
+        text = ''
+
+        for step, instruction in instructions:
+            text += f'**{step}**  {instruction}  '
+
+        if self.use_template:
+            self.template = self.template.replace('<!--instructions_list-->', text)
+        else:
+            self.markdown += text
+            self.insert_line()
 
     def insert_line(self):
         self.markdown += '\n---\n'
 
     def add_nutrition(self, nutrition: dict):
-        self.markdown += '## Nutrition'
-        self.markdown += nutrition
-        self.insert_line()
+        text = ''
+
+        for nutrition_type, amount in nutrition:
+            text += f'{nutrition_type}: {amount}  '
+
+        if self.use_template:
+            self.template = self.template.replace('<!--nutrition_list-->', text)
+        else:
+            self.markdown += '## Nutrition'
+            self.markdown += text
+            self.insert_line()
+
+    def add_original_link(self, link: str):
+        if self.use_template:
+            self.template = self.template.replace('<!--original_recipe_link-->', link)
+        else:
+            self.markdown += link
 
     def save(self, path):
         path = Path(path)
@@ -58,4 +96,7 @@ class MarkdownEditor:
             full_path = path / new_title
 
         with open(full_path, 'w') as md:
-            md.write(self.markdown)
+            if self.use_template:
+                md.write(self.template)
+            else:
+                md.write(self.markdown)
---
+++ recipe_template.md
@@ -0,0 +1,22 @@
+# <!--title-->
+
+___
+<!--tags_list-->
+<!--original_recipe_link-->
+___
+
+### Ingredients
+
+<!--ingredients_list-->
+---
+
+### Instructions
+
+**Step1**:  
+Get water boiling
+<!--instructions_list-->
+---
+
+### Nutrition
+
+<!--nutrition_list-->
